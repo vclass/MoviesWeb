937a49be07f79c1c76aedefefca4d31ac5e9aa02
NOPPARAT (TONG) LEELAVANICH <leevclass@gmail.com>
2015-11-29 16:29:59 -0800

    style: format entertainment_center.py and media.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,37 +1,39 @@
 from media import Movie
 import fresh_tomatoes
 
-#Create Toy Story Movie
 
-title = "Toy Story"
-storyline = "Toys Come to Life"
-release_date = "1995-11-22"
-rated = "G"
-poster_image_url = "http://cdn.collider.com/wp-content/uploads/toy-story-poster1.jpg"
-trailer_youtube_url = "https://www.youtube.com/watch?v=KYz2wyBy3kc"
-toy_story = Movie(title,storyline,release_date,rated,poster_image_url,trailer_youtube_url)
+# Create Toy Story Movie
 
-#Create Martian Movie
+toy_story = Movie(
+            "Toy Story",
+            "Toys Come to Life",
+            "1995-11-22",
+            "G",
+            "http://cdn.collider.com/wp-content/uploads/toy-story-poster1.jpg",
+            "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
-title = "Martian"
-storyline = "Escape from Mars"
-release_date = "2015-10-2"
-rated = "PG-13"
-poster_image_url = "http://sumnersunsettheatre.com/wp-content/uploads/The-Martian-movie-poster.jpg"
-trailer_youtube_url = "https://www.youtube.com/watch?v=Ue4PCI0NamI"
-martian = Movie(title,storyline,release_date,rated,poster_image_url,trailer_youtube_url)
+# Create Martian Movie
 
-#Create Inception Movie
+martian = Movie(
+            "Martian",
+            "Escape from Mars",
+            "2015-10-2",
+            "PG-13",
+            "http://sumnersunsettheatre.com/wp-content/uploads/The-Martian-movie-poster.jpg",		# noqa
+            "https://www.youtube.com/watch?v=Ue4PCI0NamI")
 
-title = "Inception"
-storyline = "Dive to the dream"
-release_date = "2010-7-13"
-rated = "PG-13"
-poster_image_url = "http://www.aceshowbiz.com/images/still/inception_poster01.jpg"
-trailer_youtube_url = "https://www.youtube.com/watch?v=66TuSJo4dZM"
-inception = Movie(title,storyline,release_date,rated,poster_image_url,trailer_youtube_url)
+# Create Inception Movie
 
-#Store all movies in movies list and send to fresh_tomatoes to create web page to show movies and their descriptions
+Inception = Movie(
+            "Inception",
+            "Dive to the dream",
+            "2010-7-13",
+            "PG-13",
+            "hhttp://www.aceshowbiz.com/images/still/inception_poster01.jpg",
+            "https://www.youtube.com/watch?v=66TuSJo4dZM")
 
-movies = [toy_story,martian,inception]
+# Store all movies in movies list and send to fresh_tomatoes to
+# create web page to show movies and their descriptions
+
+movies = [toy_story, martian, inception]
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,23 +1,25 @@
 import webbrowser
 
+
 class Movie():
-	"""A movie class that stores its desription and details
+    """A movie class that stores its desription and details
 
-	Attributes:
-		title: A string contains movie title name
-		storyline: A string decribes main story line of the movie
-		release_date: A string indicate movie's release date
-		rated: A string indicates movies rated e.g. G, PG, PG-13 etc.
-		poster_image_url: A link to movie's poster image
-		youtube_url: A link to movie's trailer 
-	"""
+    Attributes:
+        title: A string contains movie title name
+        storyline: A string decribes main story line of the movie
+        release_date: A string indicate movie's release date
+        rated: A string indicates movies rated e.g. G, PG, PG-13 etc.
+        poster_image_url: A link to movie's poster image
+        youtube_url: A link to movie's trailer
+    """
 
-	def __init__(self,movie_title,movie_storyline,movie_release_date,movie_rated,poster_image,trailer_youtube):
-		"""Inits Movie class with their movie's attributes"""
-		self.title = movie_title
-		self.storyline = movie_storyline
-		self.release_date = movie_release_date
-		self.rated = movie_rated
-		self.poster_image_url = poster_image
-		self.trailer_youtube_url = trailer_youtube
+    def __init__(self, movie_title, movie_storyline, movie_release_date,
+                 movie_rated, poster_image, trailer_youtube):
+        """Inits Movie class with their movie's attributes"""
 
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.release_date = movie_release_date
+        self.rated = movie_rated
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
